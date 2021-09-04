4e577b501a447e8895a3fbd6b56d66e869742d63
Anas Abdul <anasabdulwahid@gmail.com>
2021-09-04 14:11:10 +0800

    First Commit

---
+++ components/Map.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {StyleSheet, View, Text } from "react-native";
+
+const Map = () => {
+  return (
+    <View>
+      <Text>i'm a Map</Text>
+    </View>
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({});
